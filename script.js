bca5585aac0969d40a6cd64122e20978328f5bbe
David Oseni <ostamdavid1@gmail.com>
2022-07-13 22:49:48 +0100

    work on the submit button. Now it can't submit without selecting a rate

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
+const body = document.querySelector(".aaa")
 const firstBox = document.querySelector(".ratingBox")
 const secondBox = document.querySelector(".thankYouBox")
 const submitButton = document.querySelector(".aBtn")
 const ratingBtn = document.querySelectorAll(".btn")
 let rating = document.querySelector(".rate")
 
-submitButton.addEventListener('click', function(){
-    secondBox.classList.remove('hidden')
-    firstBox.style.display = 'none'
-})
+
 
 ratingBtn.forEach((btn) => {
-   btn.addEventListener('click', function(){
-    // let selected = btn.innerHTML;
-    rating.innerHTML = btn.innerHTML;
-   })
+    btn.addEventListener('click', function(){
+     // let selected = btn.innerHTML;
+     rating.innerHTML = btn.innerHTML;
+    })
+ });
+
+submitButton.addEventListener('click', function(){
+    if(!rating.innerHTML){
+        console.log('tam')
+    } else{
+        secondBox.classList.remove('hidden');
+        firstBox.style.display = 'none'
+    };
+    
 });
+
 
 
